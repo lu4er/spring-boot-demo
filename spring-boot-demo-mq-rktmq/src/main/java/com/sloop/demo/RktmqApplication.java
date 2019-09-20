8bdc7d3f8da60305e362a9dd1380ab30460b4ca0
张金志 <zhangjinzhi@shilupan.com>
2019-09-20 18:19:13 +0800

    feat:学习rocketmq

---
+++ spring-boot-demo-mq-rktmq/src/main/java/com/sloop/demo/RktmqApplication.java
@@ -0,0 +1,11 @@
+package com.sloop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RktmqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RktmqApplication.class, args);
+    }
+}
